d3114bc5f0069ebf244d330bed6c01663c854377
Milliant_Anchora_PC <1154472551@qq.com>
2014-12-17 13:36:29 +0800

    add read and readbyte

--- basic/sim_string.go
+++ basic/sim_string.go
@@ -25,4 +25,40 @@ func main(){
 	fmt.Printf("after remove head and tail blanks%s\n",strings.TrimSpace(a))
 	f:=a
 	fmt.Printf("after remove head&&tail \"The\"%v\n",strings.Trim(f,"The"))
+    
+    fmt.Println("splite by blankspace")
+    for i,g:=range strings.Fields(a){
+         fmt.Printf("%d:%v\n",i,g)
+    }
+    fmt.Println("splite by \"love\":")
+    for i,g:= range strings.Split(a,"love"){
+        fmt.Printf("%v:%v\n",i,g)
+    }
+    h:=strings.Split(a,"and")
+    fmt.Printf("slice is: %v\n",h)
+    fmt.Printf("after join:%v\n",strings.Join(h,"or"))
+    fmt.Println("READ BY ASCII")
+    var s string = "这个世界充满爱和罪恶，爱和罪恶，爱和罪恶..."
+    strslice :=make([]byte,100)
+    _,err:=strings.NewReader(s).Read(strslice)
+    if nil!=err {
+        fmt.Println("read err")
+    }else{
+        fmt.Printf("the read message is:%v\n",string(strslice))
+    }
+    //ReadByte()只读一个字节的数据
+    data,error:=strings.NewReader(s).ReadByte()
+    if nil!=error{
+        fmt.Printf("readbyte err\n")
+    }else{
+        fmt.Printf("readbyte messge is:%v\n",string(data))
+    }
+   // for i,g:=range strings.NewReader(s).Read(strslice,10){
+   //     fmt.Printf("%v:%v\n",i,g)
+   // }
+   // fmt.Println("READ BY UNICODE\n")
+   // for i,g:=range strings.NewReader(s).ReadByte(s){
+   //    fmt.Printf("%v:%v\n",i,g)
+   // }
+
 }
